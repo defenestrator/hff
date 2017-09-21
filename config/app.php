050bed995f6e5570a76a1d0dd73170a46da1b56f
Jeremy Anderson <jeremy@edos.io>
2017-09-21 11:05:51 -0600

    test broken build

--- config/app.php
+++ config/app.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'name' => env('APP_NAME', 'Hobo Fly Fishing'),
+    'name' => env('APP_NAME', 'Hobo Fly Fishing'), \\;
 
     /*
     |--------------------------------------------------------------------------
