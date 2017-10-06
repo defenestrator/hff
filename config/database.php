67030c70bf82fc227756f2c6bc483964218c87c6
Jeremy Anderson <jeremy@edos.io>
2017-10-05 20:34:54 -0600

    wtf tags?

--- config/database.php
+++ config/database.php
@@ -22,7 +22,7 @@ return [
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
             'prefix' => '',
-            'strict' => true,
+            'strict' => false,
             'engine' => null,
         ],
     ],
