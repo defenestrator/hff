3f81e6ad4fce238f3a7c56b7f5b082b5b53cf601
Jeremy Anderson <jeremy@edos.io>
2017-10-09 21:54:33 -0600

    content updates

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -82,7 +82,7 @@ Have Rod Will Travel
         <section>
             <article class="article">
                 <header style="background-color:#0b3e6f; color:white; padding:1em; border-radius:1em; margin-top:2em;">
-                <h2>Don't be fooled by the 'free advice' game</h2>
+                <h2 class="text-center">Don't be fooled by '<strong>free advice</strong>'</h2>
                 <h3 class="text-center">We don't send <strong>The 5th Dude</strong> along for the trip</h3>
                 <p class="text-center">(on your dime)</p>
                 <h4>It's that simple.</h4>
