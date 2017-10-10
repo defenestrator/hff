3adea7e860e952396402fee3788ce62735ad4750
Jeremy Anderson <jeremy@edos.io>
2017-10-10 12:03:24 -0600

    confidence ain't a river in Egypt

--- resources/assets/js/components/PublicFooter.vue
+++ resources/assets/js/components/PublicFooter.vue
@@ -5,40 +5,18 @@
         <div class="col-sm-4">
             <a href="#about"><h4>About Us?</h4></a>
             <p>
-                Whatever wisdom we have came from anglers who are our betters.
-                The ones who smell like the water they fish. You know the ones.
+                We listen to local anglers who smell like the water they fish. You know the ones.
                 They give you 'the look' when they see rods on your truck. They know who the locals are.
-                We are lucky to know a few anglers who <strong>live</strong> the water.
+                We are lucky enough to know a few anglers who <strong><em>live</em></strong> the water.
             </p>
             <h5>Forget <em>about us.</em></h5>
-
-            <hr id="hr_border2">
-            <a href="#follow"><h4 class="menu">Follow Us</h4></a>
-            <ul class="list-inline">
-                <li>
-                    <a href="https://fb.me/hoboflyfishing"> <i class="livicon" data-name="facebook" data-size="18" data-loop="true" data-c="#fff" data-hc="#757b87"></i>
-                    </a>
-                </li>
-                <li>
-                    <a href="https://twitter.com/flyfishidaho"> <i class="livicon" data-name="twitter" data-size="18" data-loop="true" data-c="#fff" data-hc="#757b87"></i>
-                    </a>
-                </li>
-                <li>
-                    <a href="https://www.instagram.com/hoboflyfishing/"> <i class="livicon" data-name="instagram" data-size="18" data-loop="true" data-c="#fff" data-hc="#757b87"></i>
-                    </a>
-                </li>
-                <li>
-                    <a href="https://www.youtube.com/channel/UCJwjEQAWMuxqnw5VT4a9zpg"> <i class="livicon" data-name="youtube" data-size="18" data-loop="true" data-c="#fff" data-hc="#757b87"></i>
-                    </a>
-                </li>
-            </ul>
         </div>
         <div class="col-sm-4">
             <newsletter-signup id="newsletter-signup"></newsletter-signup>
         </div>
         <div class="col-sm-4">
             <!-- Contact Section Start -->
-            <a href="#contact"><h4>Contact</h4></a>
+            <a href="#contact"><h4>Shout!</h4></a>
             <ul class="list-unstyled">
                 <li>289 N. Hullen Pl.</li>
                 <li>Star, ID 83669</li>
@@ -64,7 +42,26 @@
                     </a>
                 </li>
             </ul>
-            <!-- //Contact Section End -->
+            <hr id="hr_border2">
+            <a href="#follow"><h4 class="menu">Hop Aboard</h4></a>
+            <ul class="list-inline">
+                <li>
+                    <a href="https://fb.me/hoboflyfishing"> <i class="livicon" data-name="facebook" data-size="18" data-loop="true" data-c="#fff" data-hc="#757b87"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://twitter.com/flyfishidaho"> <i class="livicon" data-name="twitter" data-size="18" data-loop="true" data-c="#fff" data-hc="#757b87"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.instagram.com/hoboflyfishing/"> <i class="livicon" data-name="instagram" data-size="18" data-loop="true" data-c="#fff" data-hc="#757b87"></i>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.youtube.com/channel/UCJwjEQAWMuxqnw5VT4a9zpg"> <i class="livicon" data-name="youtube" data-size="18" data-loop="true" data-c="#fff" data-hc="#757b87"></i>
+                    </a>
+                </li>
+            </ul>
         </div>
     </div>
 </footer>
