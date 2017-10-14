6b49be19dbff3ef8b4292aa3a331f2df9a373f93
Jeremy Anderson <jeremy@edos.io>
2017-10-13 19:26:59 -0600

    CSS reduction

--- resources/views/vendor/spark/layouts/app.blade.php
+++ resources/views/vendor/spark/layouts/app.blade.php
@@ -1,19 +1,11 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-    <!-- Meta Information -->
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <title>@yield('title', config('app.name'))</title>
-
-    <!-- Fonts -->
-    <link href='https://fonts.googleapis.com/css?family=Open+Sans:300,400,600' rel='stylesheet' type='text/css'>
     <link href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css' rel='stylesheet' type='text/css'>
-
-    <!-- CSS -->
-    <link href="/css/sweetalert.css" rel="stylesheet">
     <link href="{{ mix('css/app.css') }}" rel="stylesheet">
 
     <!-- Scripts -->
