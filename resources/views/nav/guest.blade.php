02430ec798c65196e5c2636e45f30646a70fd73f
Jeremy Anderson <jeremy@edos.io>
2017-10-17 15:56:49 -0600

    what the hell

--- resources/views/nav/guest.blade.php
+++ resources/views/nav/guest.blade.php
@@ -16,53 +16,53 @@
         <div class="collapse navbar-collapse" id="spark-navbar-collapse">
             <!-- Left Side Of Navbar -->
             <ul class="nav navbar-nav">
-                <li class="dropdown thumb-dropdown">
-                    <a href="{{route('publications.posts.index')}}" class="dropdown-toggle" data-toggle="dropdown">Trips<span class="caret"></span></a>
-                    <ul class="dropdown-menu" role="menu">
-                        <li role="presentation" class="dropdown-header">Steelhead and Salmon</li>
-                        <li>
-                            <a href="trips/backcountry-river-guides">
-                                Backcountry River Guides
-                                <div class="thumbnail">
-                                    <img class="img-responsive" src="images/backcountry-river-guides/slide1-178.jpg">
-                                    <div class="caption">
-                                        <p>Roadless Wilderness Jet Boat Steelhead</p>
-                                    </div>
-                                </div>
+                {{--<li class="dropdown thumb-dropdown">--}}
+                    {{--<a href="{{route('publications.posts.index')}}" class="dropdown-toggle" data-toggle="dropdown">Trips<span class="caret"></span></a>--}}
+                    {{--<ul class="dropdown-menu" role="menu">--}}
+                        {{--<li role="presentation" class="dropdown-header">Steelhead and Salmon</li>--}}
+                        {{--<li>--}}
+                            {{--<a href="trips/backcountry-river-guides">--}}
+                                {{--Backcountry River Guides--}}
+                                {{--<div class="thumbnail">--}}
+                                    {{--<img class="img-responsive" src="/images/backcountry-river-guides/slide1-178.jpg">--}}
+                                    {{--<div class="caption">--}}
+                                        {{--<p>Roadless Wilderness Jet Boat Steelhead</p>--}}
+                                    {{--</div>--}}
+                                {{--</div>--}}
 
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                Deep Creek Lodge
-                                <div class="thumbnail">
-                                    <img class="img-responsive" src="images/gourley/bonefish.jpg">
-                                </div>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#">
-                                Flat user profile
-                                <div class="thumbnail">
-                                    <img class="img-responsive" src="images/gourley/bonefish.jpg">
-                                </div>
-                            </a>
-                        </li>
-                        <li class="divider"></li>
-                        <li role="presentation" class="dropdown-header">Thumbnail with caption</li>
-                        <li>
-                            <a href="#">
-                                Discount labels
-                                <div class="thumbnail">
-                                    <img class="img-responsive" src="images/gourley/bonefish.jpg">
-                                    <div class="caption">
-                                        <p>You can add any text for describe thumbnail here.</p>
-                                    </div>
-                                </div>
-                            </a>
-                        </li>
-                    </ul>
-                </li>
+                            {{--</a>--}}
+                        {{--</li>--}}
+                        {{--<li>--}}
+                            {{--<a href="#">--}}
+                                {{--Deep Creek Lodge--}}
+                                {{--<div class="thumbnail">--}}
+                                    {{--<img class="img-responsive" src="/images/gourley/bonefish.jpg">--}}
+                                {{--</div>--}}
+                            {{--</a>--}}
+                        {{--</li>--}}
+                        {{--<li>--}}
+                            {{--<a href="#">--}}
+                                {{--Flat user profile--}}
+                                {{--<div class="thumbnail">--}}
+                                    {{--<img class="img-responsive" src="images/gourley/bonefish.jpg">--}}
+                                {{--</div>--}}
+                            {{--</a>--}}
+                        {{--</li>--}}
+                        {{--<li class="divider"></li>--}}
+                        {{--<li role="presentation" class="dropdown-header">Thumbnail with caption</li>--}}
+                        {{--<li>--}}
+                            {{--<a href="#">--}}
+                                {{--Discount labels--}}
+                                {{--<div class="thumbnail">--}}
+                                    {{--<img class="img-responsive" src="images/gourley/bonefish.jpg">--}}
+                                    {{--<div class="caption">--}}
+                                        {{--<p>You can add any text for describe thumbnail here.</p>--}}
+                                    {{--</div>--}}
+                                {{--</div>--}}
+                            {{--</a>--}}
+                        {{--</li>--}}
+                    {{--</ul>--}}
+                {{--</li>--}}
                 <li><a href="{{route('publications.posts.index')}}" class="navbar-link dropdown">Blog</a></li>
                 <li><a href="{{route('publications.podcasts.index')}}" class="navbar-link">Podcast</a></li>
             </ul>
