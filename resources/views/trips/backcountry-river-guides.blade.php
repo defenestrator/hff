d9cb2d68de8970326860b7b74e01f9ab51b4e4d3
Jeremy Anderson <jeremy@edos.io>
2017-10-18 00:40:00 -0600

    small content tweaks

--- resources/views/trips/backcountry-river-guides.blade.php
+++ resources/views/trips/backcountry-river-guides.blade.php
@@ -14,7 +14,7 @@
 @stop
 
 @section('top')
-<img style="width:100%;"  src="{{ asset('images/backcountry-river-guides/slide1-178.jpg') }}" alt="Steelhead">
+<img style="width:100%;"  src="{{ asset('images/backcountry-river-guides/slide1-178.jpg') }}" alt="Steelhead" title="Steelhead">
     <h1 style="padding:0 0.5em;" class="showcase-header text-center wow pulse" data-wow-duration="0.5s">
         Backcountry River Guides
     </h1>
@@ -33,7 +33,7 @@
                     Some of this water has never seen a spey rod, a fact that should excite anyone in
                     search of untouched steelies!
                     </p>
-                    <img src="{{ asset('images/backcountry-river-guides/slide2-2767.jpg') }}" alt="More Steelhead.">
+                    <img src="{{ asset('images/backcountry-river-guides/slide2-2767.jpg') }}" alt="More Steelhead." title="More Steelhead.">
                     <p>
                         If you want to hook into some fierce ocean-run redbands without literally rubbing elbows with
                         people you've never met, this is the most exciting opportunity in the lower 48!
@@ -44,7 +44,7 @@
                         Get a shot at the most remote steelhead water in the continental USA, with Brent Sawyer and
                         Backcountry River Guides!
                     </p>
-                    <img src="{{ asset('images/backcountry-river-guides/steelie-hell.jpg') }}" alt="Even More Steelhead">
+                    <img src="{{ asset('images/backcountry-river-guides/steelie-hell.jpg') }}" alt="Even More Steelhead" title="Even More Steelhead">
                 </article>
             </section>
 
@@ -60,11 +60,11 @@
                     <p class="text-center"><strong>4 anglers, 4 days. Fully outfitted.</strong></p>
                     <h3 class="text-center"><i class="fa fa-bolt"></i><strong> $4000 </strong><i class="fa fa-bolt"></i></h3>
                     <sales-inquiry></sales-inquiry>
-                    <img src="{{ asset('images/backcountry-river-guides/intrudershadow1.png') }}" alt="Intruder Alert!">
+                    <img src="{{ asset('images/backcountry-river-guides/intrudershadow1.png') }}" alt="Intruder Alert!" title="Intruder Alert!">
                     <p>Backcountry River Guides' trips are part of our "Full Service" lineup and we highly
                         recommend them to any steelheaders chasing a world-class trip on a budget.</p>
 
-                    <img src="{{ asset('images/backcountry-river-guides/boat.jpg') }}" alt="BRG - Brent Sawyer">
+                    <img src="{{ asset('images/backcountry-river-guides/boat.jpg') }}" alt="BRG - Brent Sawyer - The Boat" title="BRG - Brent Sawyer - The Boat">
                    <p> We are confident this is the best steelhead expedition, at this price, in the lower 48.</p>
                 </article>
             </section>
@@ -78,9 +78,7 @@
 @stop
 {{-- footer scripts --}}
 @section('page_scripts')
-   <script>
-        new WOW().init();
-    </script>
-    {{--The Big Lebowski is a trademark and copyright of Universal Studios, used here in accordance under the Fair Use clause, title 17 section 107 USC,
-    source: http://static6.businessinsider.com/image/56b36fc72e5265b6008b4cd0-1190-625/jeff-bridges-says-hes-all-in-for-starring-in-a-big-lebowski-sequel-its-a-great-idea.jpg --}}
-@stop
+<script>
+    new WOW().init();
+</script>
+  @stop
