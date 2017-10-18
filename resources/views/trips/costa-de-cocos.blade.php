623a826457da63a11fb14513fb169e62b96b427d
Jeremy Anderson <jeremy@edos.io>
2017-10-18 10:51:50 -0600

    cocos

--- resources/views/trips/costa-de-cocos.blade.php
+++ resources/views/trips/costa-de-cocos.blade.php
@@ -26,13 +26,7 @@
                         and sometimes lead to a lack of sleep or focus during the slog of everyday life.
 
                     </p>
-                    <p class="text-center">
-                        <iframe width="560"
-                                height="315"
-                                src="https://www.youtube-nocookie.com/embed/YCEpp1A04KY?rel=0"
-                                frameborder="0" allowfullscreen>
-                        </iframe>
-                    </p>
+
                     <p>
                         As we tie flies in anticipation of our return, Permit, Bonefish and giant Barracuda race through
                         our minds with each wrap we make. In an attempt to stay connected with the waters near Xcalak
@@ -45,17 +39,26 @@
                         Abundant opportunity exists for Bonefish, Permit, Jacks, Barracuda, Tarpon, Trigger, Snapper,
                         and numerous other species that will gladly eat a fly.
                     </p>
-                    <img src="http://www.costadecocos.com/images/costa%20de%20cocos%20fishing%20lg.jpg">
+
                     <p>
                         This is a year-round opportunity for flats fishing. In some destinations anglers have been conditioned
                         to plan trips only during the "peak" months. In these waters, amazing angling can be had on almost
                         every day of the year.
-                    </p><p>
-                        When it comes to ”DiY” anglers in search of new flats to stalk, this area
+                    </p>
+                        <img src="http://www.costadecocos.com/images/costa%20de%20cocos%20fishing%20lg.jpg">
+                    <p>
+                        For saltwater ”DiY” anglers in search of new flats to stalk, this area
                         has few rivals. The thing about doing it yourself is; No matter how great you think
                         you are, a new area takes time. Success for a self-guided angler is based largely on time spent
                         working the water. And yes, at least a little luck. When you finally land a monstrous Permit,
                         or a lightning bolt of a Bonefish on some nameless flat devoid of other anglers, the victory will taste sweeter for it.
+                    </p>
+                    <p class="text-center">
+                        <iframe width="560"
+                                height="315"
+                                src="https://www.youtube-nocookie.com/embed/YCEpp1A04KY?rel=0"
+                                frameborder="0" allowfullscreen>
+                        </iframe>
                     </p>
                 </article>
             </section>
@@ -63,6 +66,9 @@
         <div class="col-sm-4 col-sm-offset-0">
             <section>
                 <article class="article">
+                    <img src="/images/costa-de-cocos/OttoMannandAlberto_640x419.jpg"
+                         alt="Otto and Alberto with a nice Permit"
+                         title="Otto and Alberto with a nice Permit">
                     <p> Is an egg-white omelet and a caffè latte at some extravagant lodge across the border worth three grand?
                     </p>
                     <hr>
@@ -71,9 +77,7 @@
                     <h3 class="text-center"><strong> $???? </strong> Drop us a line!</h3>
                     <sales-inquiry></sales-inquiry>
                     <p>The fish don't know if they are in Belize or Mexico, it's all the same water.</p>
-                    <img src="/images/costa-de-cocos/OttoMannandAlberto_640x419.jpg"
-                         alt="Otto and Alberto with a nice Permit"
-                         title="Otto and Alberto with a nice Permit">
+
                     <p>
                         Our hybrid trip includes both guided and self-guided days in this amazing location. Our exclusive angler
                         support package will save you time and reduce the learning curve while it saves you money.
