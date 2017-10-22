983d1a9cb9424169599972c352fab3db37015557
Jeremy Anderson <jeremy@edos.io>
2017-10-21 20:19:49 -0600

    fewer words

--- resources/views/destinations/naknek-river-camp.blade.php
+++ resources/views/destinations/naknek-river-camp.blade.php
@@ -48,7 +48,7 @@
                 <article class="article">
                     <p class="text-center">
                         <img src="/images/naknek-river-camp/steelie.jpg" /></p>
-                    <p class="text-center"><strong>Less costly than you can imagine.</strong></p>
+                    <p class="text-center"><strong>Less costly than you imagine.</strong></p>
                     <h3 class="text-center">We know you're curious.</h3>
                     <sales-inquiry></sales-inquiry>
                     <img class="logo" src="/images/naknek-river-camp/naknek-rivercamp-logo.png" />
