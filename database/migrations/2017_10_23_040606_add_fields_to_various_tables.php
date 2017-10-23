dc19968d6caf5065c6ac7c1652883f427045ced5
Jeremy Anderson <jeremy@edos.io>
2017-10-23 08:56:01 -0600

    migrations not null are nullable() now :)

--- database/migrations/2017_10_23_040606_add_fields_to_various_tables.php
+++ database/migrations/2017_10_23_040606_add_fields_to_various_tables.php
@@ -16,20 +16,20 @@ class AddFieldsToVariousTables extends Migration
 
         Schema::table('destinations', function (Blueprint $table) {
             $table->integer('team_id')->unsigned()->nullable();
-            $table->decimal('lat', 10, 7);
-            $table->decimal('lng', 11, 8);
-            $table->string('name');
-            $table->longText('description');
+            $table->decimal('lat', 10, 7)->nullable();
+            $table->decimal('lng', 11, 8)->nullable();
+            $table->string('name')->nullable();
+            $table->longText('description')->nullable();
         });
 
         Schema::table('packages', function(Blueprint $table){
-            $table->integer('destination_id')->unsigned();
+            $table->integer('destination_id')->unsigned()->nullable();
             $table->integer('min_anglers')->unsigned()->nullable();
             $table->integer('max_anglers')->unsigned()->nullable();
-            $table->string('type');
-            $table->string('title');
+            $table->string('type')->nullable();
+            $table->string('title')->nullable();
             $table->longText('description')->nullable();
-            $table->integer('price_per_angler');
+            $table->integer('price_per_angler')->nullable();
             $table->integer('percent_deposit_per_angler')->nullable();
             $table->longText('terms')->nullable(); //store as json object for configurability.
             $table->longText('misc_fees')->nullable(); //store as json object for configurability.
@@ -37,10 +37,10 @@ class AddFieldsToVariousTables extends Migration
         });
 
         Schema::table('expeditions', function(Blueprint $table){
-            $table->integer('package_id');
-            $table->string('type');
-            $table->integer('num_days');
-            $table->longText('description');
+            $table->integer('package_id')->nullable();
+            $table->string('type')->nullable();
+            $table->integer('num_days')->nullable();
+            $table->longText('description')->nullable();
         });
         Schema::table('newsletter_subscriptions', function(Blueprint $table) {
             $table->boolean('confirmed')->after('email_address')->default(false);
