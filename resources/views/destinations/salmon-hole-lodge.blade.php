c777f71894939e526c3fe408ae0d6a1bb15f997c
Jeremy Anderson <jeremy@edos.io>
2017-10-26 04:29:33 -0600

    alert redundant redundancy alert

--- resources/views/destinations/salmon-hole-lodge.blade.php
+++ resources/views/destinations/salmon-hole-lodge.blade.php
@@ -18,25 +18,28 @@
 @stop
 
 @section('content')
-    <h2>Conquer <em>The Leaper</em></h2>
+    <h2>Conquer The Leaper</h2>
     <div class="row">
         <div class="col-sm-8 col-sm-offset-0">
             <section>
                 <article class="article">
                     <p>At some point in your flyfishing career, you have, or will, wonder what it’s like to catch an Atlantic
-                        salmon. They’re mysterious, elusive and wrapped in legendary legend<a href="#that-was-a-joke" title="Intentional foul!">*</a>. They’re also usually really
-                        freaking expensive to fish for because many of the rivers with them have been tied up for generations
+                        salmon. They’re mysterious, elusive and wrapped in legendary legend<a href="#that-was-a-joke"
+                                                                                              title="Intentional foul!">*</a>.
+                        They’re also usually really freaking expensive to fish for because many of the rivers with them have been tied up for generations
                         by a few lodges that could drain a Harvard-sized college fund.</p>
-                    <img style="width:100%;"  src="{{asset('images/salmon-hole-lodge/rainbow.jpg')}}" alt="Rainbows lead to a pot of silver" title="Rainbows lead to a pot of silver">
+                    <img style="width:100%;"  src="{{asset('images/salmon-hole-lodge/rainbow.jpg')}}"
+                         alt="Rainbows lead to a pot of silver" title="Rainbows lead to a pot of silver">
 
                     <p>We’ve found a chink in their armor. The Salmon Hole Lodge offers affordable Atlantic salmon fishing,
                         which are words rarely seen together. The lodge sets 3 miles upstream from the Atlantic on the
                         Lapoile River in Newfoundland. The river offers fishing for Atlantic salmon and sea-run brook trout.
                         All fishing is guided, as required by law.</p>
 
-                    <p>The Salmon Hole Lodge is a rustic fishing camp with clean and comfortable accommodations. Located
-                        three miles upstream from the Atlantic, it’s centrally situated among the La Poile’s finest salmon pools.
-                        The lodge is spacious and caters to no more than six anglers per week to ensure an uncrowded conditions with
+                    <p>The Salmon Hole Lodge is a rustic fishing camp with clean and comfortable accommodations.
+                        It’s centrally situated among the La Poile’s finest salmon pools.
+                        The lodge is spacious and caters to no more than six anglers per week to ensure an
+                        uncrowded conditions with
                         low angling pressure and optimal catch rates.</p>
                     <img style="width:100%;"  src="{{asset('images/salmon-hole-lodge/canada-tattoo.jpg')}}"
                          alt="Keep them wet and let them go" title="Keep them wet and let them go">
