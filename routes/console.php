8b5989711804b61ce99d831399b809fb582cec76
Jeremy Anderson <jeremy@edos.io>
2017-11-05 18:44:20 -0700

    updated meta stuff and working on image processing commands.

--- routes/console.php
+++ routes/console.php
@@ -13,27 +13,27 @@ use Intervention\Image\Facades\Image;
 |
 */
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->describe('Display an inspiring quote');
+//Artisan::command('inspire', function () {
+//    $this->comment(Inspiring::quote());
+//})->describe('Display an inspiring quote');
 
-Artisan::command('image-processing', function() {
-    $this->comment('Resizing new images and pushing to DO spaces.');
-    resizing(public_path('images'));
-})->describe('Resize and store assets on DO object store');
+//Artisan::command('image-processing', function() {
+//    $this->comment('Resizing new images and pushing to DO spaces.');
+//    resizing(public_path('images'));
+//})->describe('Resize and store assets on DO object store');
 
-function resizing( $folder )
-{
-    foreach( scandir( $folder ) as $file )
-    {
-        $filepath = $folder . '/' . $file;
-
-        if( preg_match( '/^\./', $file ) ) continue; // not . and ..
-        if( is_dir( $filepath ) ) resizing( $filepath ); // Send it off to drill - with this function!!
-        // It's time to resize
-        echo($filepath . "\r\n");
-        $img = Image::make($filepath);
-        $img->resize(300, 200)->save(config('filesystems.cloud'));
-    };
-
-};
+//function resizing( $folder )
+//{
+//    foreach( scandir( $folder ) as $file )
+//    {
+//        $filepath = $folder . '/' . $file;
+//
+//        if( preg_match( '/^\./', $file ) ) continue; // not . and ..
+//        if( is_dir( $filepath ) ) resizing( $filepath ); // Send it off to drill - with this function!!
+//        // It's time to resize
+//        echo($filepath . "\r\n");
+//        $img = Image::make($filepath);
+//        $img->resize(300, 200)->save(config('filesystems.cloud'));
+//    };
+//
+//};
