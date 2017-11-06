cbd9aa97c4679fda1ac655e50ad1cc4d33707043
Jeremy Anderson <jeremy@edos.io>
2017-11-06 15:58:10 -0700

    un-inline all the things

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -5,22 +5,21 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>@yield('title', config('app.name'))</title>
-    <style>
-        {!!include( public_path('/css/styles.css'))  !!}
-    </style>
-    @if(config('app.env') == 'production')
-        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-107298455-1"></script>
+    {{--<link href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css' rel='stylesheet' type='text/css'>--}}
+    <link href="{{ mix('/css/styles.css') }}" rel="stylesheet">
+    {{--<!-- Global site tag (gtag.js) - Google Analytics -->--}}
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-107298455-1"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
 
-        <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag(){dataLayer.push(arguments);}
-            gtag('js', new Date());
+        gtag('config', 'UA-107298455-1');
+    </script>
 
-            gtag('config', 'UA-107298455-1');
-        </script>
-    @endif
     <!-- Scripts -->
     @yield('scripts', '')
+
     <!-- Global Spark Object -->
     <script>
         window.Spark = <?php echo json_encode(array_merge(
@@ -62,10 +61,10 @@
         @endif
     </div>
     <!-- JavaScript -->
-    <script>
-        {!!include( public_path('/js/script.js'))  !!}
-    </script>
+    <script src="{{ mix('js/script.js') }}"></script>
+    @if(config('app.env') == 'production')
 
+    @endif
     @yield('page_scripts')
 </body>
 </html>
