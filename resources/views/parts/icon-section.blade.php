d87e3031812748f76aaa40929eedd488dcf55e7d
Jeremy Anderson <jeremy@edos.io>
2017-11-06 23:35:20 -0700

    slicker

--- resources/views/parts/icon-section.blade.php
+++ resources/views/parts/icon-section.blade.php
@@ -2,30 +2,30 @@
     <div class="container">
         <ul class="list-inline">
             <li>
-                <a href="https://fb.me/hoboflyfishing"> <i class="livicon" data-name="facebook" data-size="18" data-loop="true" data-c="#fff" data-hc="#757b87"></i>
+                <a href="https://fb.me/hoboflyfishing"> <i class="livicon" data-name="facebook" data-size="18" data-loop="true" data-c="#fff" data-hc="#eaeafa"></i>
                 </a>
             </li>
             <li>
-                <a href="https://twitter.com/flyfishidaho"> <i class="livicon" data-name="twitter" data-size="18" data-loop="true" data-c="#fff" data-hc="#757b87"></i>
+                <a href="https://twitter.com/flyfishidaho"> <i class="livicon" data-name="twitter" data-size="18" data-loop="true" data-c="#fff" data-hc="#eaeafa"></i>
                 </a>
             </li>
             <li>
-                <a href="https://www.instagram.com/hoboflyfishing/"> <i class="livicon" data-name="instagram" data-size="18" data-loop="true" data-c="#fff" data-hc="#757b87"></i>
+                <a href="https://www.instagram.com/hoboflyfishing/"> <i class="livicon" data-name="instagram" data-size="18" data-loop="true" data-c="#fff" data-hc="#eaeafa"></i>
                 </a>
             </li>
             <li>
-                <a href="https://www.youtube.com/channel/UCJwjEQAWMuxqnw5VT4a9zpg"> <i class="livicon" data-name="youtube" data-size="18" data-loop="true" data-c="#fff" data-hc="#757b87"></i>
+                <a href="https://www.youtube.com/channel/UCJwjEQAWMuxqnw5VT4a9zpg"> <i class="livicon" data-name="youtube" data-size="18" data-loop="true" data-c="#fff" data-hc="#eaeafa"></i>
                 </a>
             </li>
             <li class="pull-right">
                 <ul class="list-inline icon-position">
                     <li>
-                        <a href="{{ URL::to('contact') }}"><i class="livicon" data-name="mail" data-size="18" data-loop="true" data-c="#fff" data-hc="#fff"></i></a>
-                        <label class="hidden-xs"> <a href="{{ URL::to('contact') }}" class="contact-link">info@hoboflyfishing.com</a></label>
+                        <a  href="{{ URL::to('contact') }}"><i class="livicon" data-name="mail" data-size="18" data-loop="true" data-c="#fff" data-hc="#fff"></i>
+                        <label style="color:#fff;" class="hidden-xs"> info@hoboflyfishing.com</label></a>
                     </li>
                     <li>
-                        <a href="tel:1-208-859-9133"><i class="livicon" data-name="phone" data-size="18" data-loop="true" data-c="#fff" data-hc="#fff"></i></a>
-                        <label class="hidden-xs"><a href="tel:1-208-859-9133" class="contact-link">(208)859-9133</a></label>
+                        <a href="tel:1-208-859-9133"><i class="livicon" data-name="phone" data-size="18" data-loop="true" data-c="#fff" data-hc="#fff"></i>
+                        <label style="color:#fff;"  class="hidden-xs">(208)859-9133</label></a>
                     </li>
                 </ul>
             </li>
