12b854f51a9a445d88f0735b17df0c585ec07414
Jeremy Anderson <jeremy@edos.io>
2017-11-07 19:56:40 -0700

    Tests? Maybe?

--- app/Http/Controllers/DeploymentController.php
+++ app/Http/Controllers/DeploymentController.php
@@ -18,16 +18,24 @@ class DeploymentController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
+     * @param Request $request
+     * @return \Illuminate\Contracts\Routing\ResponseFactory|\Symfony\Component\HttpFoundation\Response
      */
     public function create(Request $request)
     {
 //        $request->validate([
 //            $request => 'json|required'
 //        ]);
-        Deployment::create(['payload' => $request->all()]);
+        Deployment::create(['payload' => $request]);
+        // create a new cURL resource
+        $ch = curl_init();
+        // set URL and other appropriate options
+        curl_setopt($ch, CURLOPT_URL, config('app.deploy'));
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        // grab URL and pass it to the browser
+        curl_exec($ch);
+        // close cURL resource, and free up system resources
+        curl_close($ch);
         return response('OK', 200);
     }
 
