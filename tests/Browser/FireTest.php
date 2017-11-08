b4983d1fe49020a4e355cad2005fe8463e2851f5
Jeremy Anderson <jeremy@edos.io>
2017-11-07 22:03:12 -0700

    Goddamn you to hell CircleCI

--- tests/Browser/FireTest.php
+++ tests/Browser/FireTest.php
@@ -17,7 +17,7 @@ class FireTest extends DuskTestCase
         $this->browse(function ($browser) {
             $browser->visit(new HomePage)
                 ->waitForText('Hobo')
-                ->assertTitleContains('Hobo');
+                ->assertTitleContains('Have Rod, Will Travel');
         });
     }
 
