0ed2ac7b2a36c78e16110d5194503d39bce98a2e
Jeremy Anderson <jeremy@edos.io>
2017-11-15 09:23:43 -0700

    checkpoint

--- resources/views/parts/bottom.blade.php
+++ resources/views/parts/bottom.blade.php
@@ -1,5 +1,28 @@
 <div class="copyright">
     <div class="container">
+        <div class="payment-types"><ul class="inline">
+
+                <li class="small">
+                    <img src="{{config('filesystems.disks.DO.fullpath')}}/images/payment-types/blah.png">
+                </li>
+
+
+            </ul>
+        </div>
+    </div>
+    <div class="container">
+        <div class="payment-types"><ul class="inline">
+                <li class="small" style="color:white;">Electronic payments are processed using state-of-the-art security
+                    and end-to-end encryption, through <a href="https://stripe.com" target="_blank">Stripe.com</a></li>
+                <li class="small">There are literally too many to list, but we'll add some
+                        cool icons here soon to make you feel comfortable.</li>
+            </ul>
+        </div>
+    </div>
+</div>
+
+<div class="copyright">
+    <div class="container">
         <div class="legal"><ul class="inline">
                 <li class="small">&copy; Hobo Fly Fishing, LLC - {{ date('M d, Y') }}</li>
                 <li class="small footer-link"><a href="{{ route('terms') }}">Terms</a>
