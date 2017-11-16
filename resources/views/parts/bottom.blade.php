a43d6d262fb1339680966869c38842630dfbf151
Jeremy Anderson <jeremy@edos.io>
2017-11-16 07:56:03 -0700

    checkpoint

--- resources/views/parts/bottom.blade.php
+++ resources/views/parts/bottom.blade.php
@@ -1,21 +1,56 @@
 <div class="copyright">
     <div class="container">
         <div class="payment-types"><ul class="inline">
-
-                <li class="small">
-                    <img src="{{config('filesystems.disks.DO.fullpath')}}/images/payment-types/blah.png">
+                <li class="small" >We accept all major credit cards, Paypal, ACH, BitCoin, and many others:</li>
+            </ul>
+        </div>
+    </div>
+    <div class="container">
+        <div class="payment-types"><ul class="inline payment-icons">
+                <li>
+                    <img src="{{config('filesystems.disks.DO.fullpath')}}/images/payment-types/american_express.png"
+                        alt="We accept American Express" title="We accept American Express">
                 </li>
-
-
+                <li>
+                    <img src="{{config('filesystems.disks.DO.fullpath')}}/images/payment-types/visa.png"
+                         alt="We accept Visa" title="We accept Visa">
+                </li>
+                <li>
+                    <img src="{{config('filesystems.disks.DO.fullpath')}}/images/payment-types/mastercard.png"
+                         alt="We accept Mastercard" title="We accept Mastercard">
+                </li>
+                <li>
+                    <img src="{{config('filesystems.disks.DO.fullpath')}}/images/payment-types/discover.png"
+                         alt="We accept Discover" title="We accept Discover">
+                </li>
+                <li>
+                    <img src="{{config('filesystems.disks.DO.fullpath')}}/images/payment-types/diners_club.png"
+                         alt="We accept Diners Club" title="We accept Diners Club">
+                </li>
+                <li>
+                    <img src="{{config('filesystems.disks.DO.fullpath')}}/images/payment-types/paypal.png"
+                         alt="We accept Paypal" title="We accept Paypal">
+                </li>
+                <li>
+                    <img src="{{config('filesystems.disks.DO.fullpath')}}/images/payment-types/ach-payment.png"
+                         alt="We accept ACH Payments" title="We accept ACH Payments">
+                </li>
+                <li>
+                    <img src="{{config('filesystems.disks.DO.fullpath')}}/images/payment-types/currency_bitcoin_sign.png"
+                         alt="We accept Bitcoin!" title="We accept Bitcoin!">
+                </li>
+                <li>
+                    <img src="{{config('filesystems.disks.DO.fullpath')}}/images/payment-types/currency_euro_sign.png"
+                         alt="We accept Euros, and more than 130 other currencies!"
+                         title="We accept Euros, and more than 130 other currencies!">
+                </li>
             </ul>
         </div>
     </div>
     <div class="container">
         <div class="payment-types"><ul class="inline">
-                <li class="small" style="color:white;">Electronic payments are processed using state-of-the-art security
-                    and end-to-end encryption, through <a href="https://stripe.com" target="_blank">Stripe.com</a></li>
-                <li class="small">There are literally too many to list, but we'll add some
-                        cool icons here soon to make you feel comfortable.</li>
+                <li class="small" >We adhere to proven security and end-to-end encryption practices, through our partnership with Stripe.com</li>
+
             </ul>
         </div>
     </div>
