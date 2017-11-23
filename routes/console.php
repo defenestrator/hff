dffb891b2ef6cf9d56277630b3fbe62cee622b24
Jeremy Anderson <jeremy@edos.io>
2017-11-22 22:37:48 -0700

    thumbnail and image processing for showcases

--- routes/console.php
+++ routes/console.php
@@ -20,35 +20,7 @@ Artisan::command('sitemap', function () {
     $this->comment('Sitemap Generated!');
 })->describe('Generate an updated sitemap for teh googlebots pleasure, slave.');
 
-Artisan::command('podfeed', function () {
-    $this->comment('Fetching Podcast feed from fireside.fm');
-    $client = new Client();
-    $response = $client->request('GET','https://have-rod-will-travel.fireside.fm/json');
-    if ($response->getStatusCode() == 200 && $response !== null && $response->getHeader('Content-Length') > '0') {
-        $key = 'podcast_feed-'.date('Y-m-d');
-        Cache::put($key, json_decode($response->getBody(), true), 4320);
-        return $this->comment('Podcast Feed key "' . $key . '" was updated');
-    }
-    return $this->comment('Podcast Feed failed to update');
-})->describe('Update Podcast Feed, store to Redis.');
-
 Artisan::command('image-processing', function() {
-    $this->comment('Resizing new images and pushing to DO spaces.');
-    resizing(public_path('images'));
-})->describe('Resize and store assets on DO object store');
+    $this->comment('Resize and store images');
 
-function resizing( $folder )
-{
-    foreach( scandir( $folder ) as $file )
-    {
-        $filepath = $folder . '/' . $file;
-
-        if( preg_match( '/^\./', $file ) ) continue; // not . and ..
-        if( is_dir( $filepath ) ) resizing( $filepath ); // Send it off to drill - with this function!!
-        // It's time to resize
-        echo($filepath . "\r\n");
-        $img = Image::make($filepath);
-        $img->resize(300, 200)->save(config('filesystems.cloud'));
-    };
-
-};
+})->describe('Resize and store images');
