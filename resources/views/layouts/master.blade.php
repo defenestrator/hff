13a9c063323148327f0ffa963c99ab26ba979291
Jeremy Anderson <jeremy@edos.io>
2017-11-28 08:01:36 -0700

    favicon-57.png - better logged in logo

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -13,6 +13,7 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="/favicon-57.png" sizes="57x57">
     <title>@yield('title', config('app.name'))</title>
     <link href='https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css' rel='stylesheet' type='text/css'>
     <link href="{{ mix('/css/styles.css') }}" rel="stylesheet">
