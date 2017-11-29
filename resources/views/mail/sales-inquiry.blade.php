f9f370615942decf6b06f493e996877a4574cced
Jeremy Anderson <jeremy@edos.io>
2017-11-29 11:49:31 -0700

    awesomer sales contact form

--- resources/views/mail/sales-inquiry.blade.php
+++ resources/views/mail/sales-inquiry.blade.php
@@ -2,6 +2,8 @@
     <h1>Yo dudes,</h1>
     <h2>you got a new sales inquiry!</h2>
     <div style="border:1px solid gray; padding:12px;">
+        <h3>Interested in:</h3>
+        <p>{{ $data->message }}</p>
         <h3>Email:</h3>
         @if($data->address)
         <p>{{ $data->address }}</p>
@@ -11,8 +13,6 @@
         @if ($data->name)
         <p>Name: {{ $data->name }}</p>
         @endif
-        <h3>Interested in:</h3>
-        <p>{{ $data->message }}</p>
     </div>
     <h2>Write them back!</h2>
 </div>
