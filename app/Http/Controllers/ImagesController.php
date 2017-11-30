96840f78b59ba2ee16978264ebf164f6179f7080
Jeremy Anderson <jeremy@edos.io>
2017-11-30 09:22:06 -0700

    Super fancy image manipulation codes, lolololololol... ahem, lol.

--- app/Http/Controllers/ImagesController.php
+++ app/Http/Controllers/ImagesController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Storage;
+use App\Image as ImageModel;
 use Intervention\Image\Facades\Image;
 
 class ImagesController extends Controller
@@ -11,23 +11,56 @@ class ImagesController extends Controller
     public function create(Request $request)
     {
         $request->validate([
-            'header_photo' => 'required|mimes:jpg,jpeg,png,gif|dimensions:min_width=575,max_width=2800,min_height=575,max_height=2800'
+            'header_photo' => 'required|mimes:jpg,jpeg,png'
         ]);
 
-        $image = Storage::disk('public')->putFile('images', $request->file('header_photo'));
+        $thumbnail = $this->thumbnail($request->file('header_photo'));
+
+        $stamp = $this->stamp($request->file('header_photo'));
+
         $resize = Image::make($request->file('header_photo'))
+            ->resize(1280, 1280, function ($constraint) {
+                $constraint->aspectRatio();
+                $constraint->upsize();
+            })->encode('jpg');
+        $hash = md5($resize->__toString());
+        $path = storage_path('app/public/images/' . "{$hash}.jpg");
+        $resize->save($path);
+        $header_photo = url('storage/images/' . "{$hash}.jpg");
+        ImageModel::create([
+            'thumbnail' => $thumbnail,
+            'stamp' => $stamp,
+            'large' => $header_photo
+        ]);
+        return response()->json([
+            'thumbnail' => $thumbnail,
+            'stamp' => $stamp,
+            'header_photo' => url('storage/images/' . "{$hash}.jpg"),
+            'success' => true,
+        ]);
+    }
+
+    public function thumbnail($img)
+    {
+        $resize = Image::make($img)
             ->resize(575, 575, function ($constraint) {
                 $constraint->aspectRatio();
             })->encode('jpg');
         $hash = md5($resize->__toString());
         $thumbpath = storage_path('app/public/images/' . "{$hash}.jpg");
         $resize->save($thumbpath);
-        return response()->json([
-            'thumbnail' => url('storage/images/'. "{$hash}.jpg"),
-            'header_photo' => Storage::disk('public')->url($image),
-            'success' => true,
-        ]);
+        return url('storage/images/' . "{$hash}.jpg");
     }
 
-
+    public function stamp($img)
+    {
+        $resize = Image::make($img)
+            ->resize(200, 200, function ($constraint) {
+                $constraint->aspectRatio();
+            })->encode('jpg');
+        $hash = md5($resize->__toString());
+        $thumbpath = storage_path('app/public/images/' . "{$hash}.jpg");
+        $resize->save($thumbpath);
+        return url('storage/images/' . "{$hash}.jpg");
+    }
 }
