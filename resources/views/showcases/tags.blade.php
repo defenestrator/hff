8f65c292e280cffb116f1d6b62c71e5e238c468f
Jeremy Anderson <jeremy@edos.io>
2017-12-01 10:50:59 -0700

    formatting

--- resources/views/showcases/tags.blade.php
+++ resources/views/showcases/tags.blade.php
@@ -27,32 +27,32 @@
             @if(count($showcases) == 0)
                 <h2>Sorry, there are no showcases with the '{{$tag}}' tag</h2>
             @endif
-                @foreach($showcases as $showcase)
-                    <div class="showcase">
-                        <div class="featured-text relative-left">
-                            <h2 class="primary"><a href="/showcases/{{$showcase->slug}}">{{$showcase->title}}</a></h2>
-                            <a href="/showcases/{{$showcase->slug}}">
-                                <img style="width:100%;" src="{{$showcase->header_photo}}" alt="{{$showcase->title}}" title="{{$showcase->title}}">
+            @foreach($showcases as $showcase)
+                <div class="showcase">
+                    <div class="featured-text relative-left">
+                        <h2 class="primary"><a href="/showcases/{{$showcase->slug}}">{{$showcase->title}}</a></h2>
+                        <a href="/showcases/{{$showcase->slug}}">
+                            <img style="width:100%;" src="{{$showcase->header_photo}}" alt="{{$showcase->title}}" title="{{$showcase->title}}">
+                        </a>
+                        <hr>
+                        <article>
+                            <a style="color:rgb(83, 89, 101);" href="/showcases/{{$showcase->slug}}">
+                                {!! str_limit(strip_tags($showcase->body), 200, '...') !!}
                             </a>
-                            <hr>
-                            <article>
-                                <a style="color:rgb(83, 89, 101);" href="/showcases/{{$showcase->slug}}">
-                                    {!! str_limit(strip_tags($showcase->body), 200, '...') !!}
-                                </a>
-                            </article>
-                            <br>
-                            <p class="additional-showcase-wrap">
-                            <span class="taglinks">Tags:
-                                @forelse($showcase->tags as $tag)
-                                    <a href="{{ URL::to('showcases/'. $tag->normalized .'/tag') }}">{{ $tag->name }}</a>,
-                                @empty
-                                    No Tags
-                                @endforelse
-                            </span>
-                            </p>
-                        </div>
+                        </article>
+                        <br>
+                        <p class="additional-showcase-wrap">
+                        <span class="taglinks">Tags:
+                            @forelse($showcase->tags as $tag)
+                                <a href="{{ URL::to('showcases/'. $tag->normalized .'/tag') }}">{{ $tag->name }}</a>,
+                            @empty
+                                No Tags
+                            @endforelse
+                        </span>
+                        </p>
                     </div>
-                @endforeach
+                </div>
+            @endforeach
         </div>
     <div class="col-md-4 col-md-offset-0">
 
