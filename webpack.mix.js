b1ef1ae79d6d2969785cbc44b0657db35dfe8b3b
Jeremy Anderson <jeremy@edos.io>
2017-12-28 07:33:03 -0700

    remove redundant images

--- webpack.mix.js
+++ webpack.mix.js
@@ -26,7 +26,6 @@ mix.webpackConfig({
             './resources/assets/less/sweetalert2.min.css',
             './resources/assets/less/animate.min.css',
         ], 'public/css/libs.css')
-    mix.copy('resources/assets/images', 'public/images', false)
     if(process.env.APP_ENV === "production"){
         mix.combine(['public/js/app.js','public/js/hoboscript.js'], 'public/js/script.js')
         .combine(['public/css/app.css','public/css/libs.css' ], 'public/css/styles.css')
