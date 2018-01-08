11a6932731956aefce2058c2a4a85700490e429d
Jeremy Anderson <jeremy@edos.io>
2018-01-08 09:07:16 -0700

    backups

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -23,8 +23,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-//         $schedule->command('podfeed')
-//                  ->daily();
+         $schedule->command('backup:run')->environments(['production'])->twiceDaily();
     }
 
     /**
