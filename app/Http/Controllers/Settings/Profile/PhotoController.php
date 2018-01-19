8806748bb37241c8041ac698c83bac36a6ebcbad
Jeremy Anderson <jeremy@edos.io>
2018-01-19 08:10:36 -0700

    Move profile photos to s3

--- app/Http/Controllers/Settings/Profile/PhotoController.php
+++ app/Http/Controllers/Settings/Profile/PhotoController.php
@@ -2,9 +2,11 @@
 
 namespace App\Http\Controllers\Settings\Profile;
 
+use Intervention\Image\ImageManager;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
 use Illuminate\Http\Request;
 use Laravel\Spark\Http\Controllers\Controller;
-use Laravel\Spark\Contracts\Interactions\Settings\Profile\UpdateProfilePhoto;
 
 class PhotoController extends Controller
 {
@@ -13,8 +15,20 @@ class PhotoController extends Controller
      *
      * @return void
      */
-    public function __construct()
+    /**
+     * The image manager instance.
+     *
+     * @var ImageManager
+     */
+    protected $images;
+
+    /**
+     * PhotoController constructor.
+     * @param ImageManager $images
+     */
+    public function __construct(ImageManager $images)
     {
+        $this->images = $images;
         $this->middleware('auth');
     }
 
@@ -26,9 +40,58 @@ class PhotoController extends Controller
      */
     public function store(Request $request)
     {
-        $this->interaction(
-            $request, UpdateProfilePhoto::class,
-            [$request->user(), $request->all()]
+        $this->handle(
+            $request->user(), $request->all()
         );
+    }
+    /**
+     * {@inheritdoc}
+     */
+    public function validator(array $data)
+    {
+        return Validator::make($data, [
+            'photo' => 'required|image|max:4000',
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function handle($user, array $data)
+    {
+        $file = $data['photo'];
+
+        $path = $file->hashName('images/profiles');
+
+        // We will store the profile photos on the "public" disk, which is a convention
+        // for where to place assets we want to be publicly accessible. Then, we can
+        // grab the URL for the image to store with this user in the database row.
+        $disk = Storage::disk('s3');
+
+        $disk->put(
+            $path, $this->formatImage($file), 'public'
+        );
+
+        $oldPhotoUrl = $user->photo_url;
+
+        $user->forceFill([
+            'photo_url' => $disk->url($path),
+        ])->save();
+
+        if (preg_match('/images\/profiles\/(.*)$/', $oldPhotoUrl, $matches)) {
+            $disk->delete('images/profiles/'.$matches[1]);
+        }
+    }
+
+    /**
+     * Resize an image instance for the given file.
+     *
+     * @param  \SplFileInfo  $file
+     * @return \Intervention\Image\Image
+     */
+    protected function formatImage($file)
+    {
+        return (string) $this->images->make($file->path())
+            ->fit(300)->encode();
     }
 }
--- app/Http/Controllers/Settings/Teams/TeamPhotoController.php
+++ app/Http/Controllers/Settings/Teams/TeamPhotoController.php
@@ -2,19 +2,30 @@
 
 namespace App\Http\Controllers\Settings\Teams;
 
+use Intervention\Image\ImageManager;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Http\Request;
 use Laravel\Spark\Http\Controllers\Controller;
-use Laravel\Spark\Contracts\Interactions\Settings\Teams\UpdateTeamPhoto;
-use Laravel\Spark\Http\Requests\Settings\Teams\UpdateTeamPhotoRequest;
 
 class TeamPhotoController extends Controller
 {
     /**
-     * Create a new controller instance.
+     * The image manager instance.
      *
+     * @var ImageManager
+     */
+    protected $images;
+
+    /**
+     * Create a new interaction instance.
+     *
+     * @param  ImageManager  $images
      * @return void
      */
-    public function __construct()
+    public function __construct(ImageManager $images)
     {
+        $this->images = $images;
         $this->middleware('auth');
     }
 
@@ -25,11 +36,61 @@ class TeamPhotoController extends Controller
      * @param  \Laravel\Spark\Team  $team
      * @return Response
      */
-    public function update(UpdateTeamPhotoRequest $request, $team)
+    public function update(Request $request, $team)
     {
-        $this->interaction(
-            $request, UpdateTeamPhoto::class,
-            [$team, $request->all()]
+        $this->handle(
+            $team, $request->all()
         );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function validator(array $data)
+    {
+        return Validator::make($data, [
+            'photo' => 'required|image|max:4000',
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function handle($team, array $data)
+    {
+        $file = $data['photo'];
+
+        $path = $file->hashName('images/teams');
+
+        // We will store the profile photos on the "public" disk, which is a convention
+        // for where to place assets we want to be publicly accessible. Then, we can
+        // grab the URL for the image to store with this user in the database row.
+        $disk = Storage::disk('s3');
+
+        $disk->put(
+            $path, $this->formatImage($file), 'public'
+        );
+
+        $oldPhotoUrl = $team->photo_url;
+
+        $team->forceFill([
+            'photo_url' => $disk->url($path),
+        ])->save();
+
+        if (preg_match('/images\/teams\/(.*)$/', $oldPhotoUrl, $matches)) {
+            $disk->delete('images/teams/'.$matches[1]);
+        }
+    }
+
+    /**
+     * Resize an image instance for the given file.
+     *
+     * @param  \SplFileInfo  $file
+     * @return \Intervention\Image\Image
+     */
+    protected function formatImage($file)
+    {
+        return (string) $this->images->make($file->path())
+            ->fit(300)->encode();
     }
 }
