f9c778f318ee1d220213dd38fba628b9e0410eb1
Jeremy Anderson <jeremy@edos.io>
2018-01-20 20:45:39 -0700

    better regions pages

--- resources/views/regions/show.blade.php
+++ resources/views/regions/show.blade.php
@@ -34,14 +34,44 @@
 @stop
 @section('content')
     <div class="row">
-        <div class="col-sm-8 col-sm-offset-0">
+        <div class="col-sm-10 col-sm-offset-1">
             <section>
                 <article class="article">
                     <h2>Places we book in {{$region->name}}</h2>
                     @foreach($region->showcases as $showcase)
-                    <ul class="list-unstyled">
-                        <li><a href="/showcases/{{ $showcase->slug }}"><h3>{{$showcase->title}}</h3><img src="{{$showcase->header_photo}}"/></a></li>
-                    </ul>
+                    <div style="padding-bottom: 24px; border-bottom:2px solid #c5c7ca;" class="article">
+                        <p><a href="/showcases/{{ $showcase->slug }}"><h3>{{$showcase->title}}</h3><img src="{{$showcase->header_photo}}"/></a></p>
+                        <article>
+                            <div class="row">
+                                <div class="col-md-4">
+                                    <h4>
+                                        <a href="/showcases/{{ $showcase->slug}}" title="{{$showcase->homepage_top}}">{{$showcase->tagline}}</a>
+                                    </h4>
+                                </div>
+                                <div class="col-md-8">
+                                    <p>
+                                        <a style="color:rgb(83, 89, 101);" href="/showcases/{{$showcase->slug}}" title="{{$showcase->homepage_bottom}}">
+                                            {!! str_limit(strip_tags($showcase->body), 200, '...') !!}
+                                        </a>
+                                    </p>
+                                    <p>
+                                        <span class="taglinks">Tags:
+                                            @forelse($showcase->tags as $tag)
+                                                <a href="{{ URL::to('showcases/'. $tag->normalized .'/tag') }}">{{ $tag->name }}</a>,
+                                            @empty
+                                                No Tags
+                                            @endforelse
+                                    </span>
+                                    </p>
+                                </div>
+                            </div>
+                        </article>
+                        <div class="row">
+                            <div class="col-md-10 col-md-offset-1">
+
+                            </div>
+                        </div>
+                    </div>
                     @endforeach
                 </article>
             </section>
