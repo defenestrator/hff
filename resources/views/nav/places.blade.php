6e84dcd25e40a463f698a207543fdf8bdc5a5c9a
Jeremy Anderson <jeremy@edos.io>
2018-01-20 22:54:47 -0700

    anchor linksies

--- resources/views/nav/places.blade.php
+++ resources/views/nav/places.blade.php
@@ -1,6 +1,12 @@
 <li class="dropdown">
     <a class="dropdown-toggle" data-toggle="dropdown" title="showcases">Places<span class="caret"></span></a>
     <ul class="scrollable-menu dropdown-menu" role="menu">
+        <li>
+            <a class="text-center" style="font-size:18px; font-weight:600;" href="/regions" title="All regions">
+                <i class="fa fa-globe" aria-hidden="true"></i> All Regions
+            </a>
+        </li>
+        <li class="divider"></li>
         @foreach($regions as $region)
             @if($region->id != 0)
                 <li>
