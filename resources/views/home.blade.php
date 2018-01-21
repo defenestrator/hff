50cfd295efdfb6df9b84880a76746c01a2bc736b
Jeremy Anderson <jeremy@edos.io>
2018-01-20 17:19:34 -0700

    why u no update?

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -11,7 +11,7 @@
 @section('top')
     <div class="tagline text-center" style="padding:8em 0; background-image:url('https://hobo-assets.s3-us-west-2.amazonaws.com/images/closeup-bone.jpg'); background-position:right center; background-size:cover;">
         <h1 style="text-shadow:#000 1px 1px 3px; font-weight:600;">
-            Fly Fishing Travel
+            Fly Fishing Planet Earth
         </h1>
         <h2 style="text-shadow:#000 1px 1px 3px;  font-weight:600;">
             Elite Destinations, Local Flavor
