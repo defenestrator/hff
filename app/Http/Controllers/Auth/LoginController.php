ba819b6e03df2eed58fd7d1bc08698a880cbfba0
Jeremy Anderson <jeremy@edos.io>
2018-04-17 18:43:20 -0600

    fuck you

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -42,6 +42,6 @@ class LoginController extends Controller
      */
     public function login()
     {
-        return 'login denied';
+        return 'login unavailable until next board meeting';
     }
 }
