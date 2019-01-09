cc458833a7f7fe08309fb9b37702bb0c0717aeef
Jeremy Anderson <jeremy@edos.io>
2019-01-09 11:19:29 -0700

    remove podcast nav link

--- resources/views/nav/guest.blade.php
+++ resources/views/nav/guest.blade.php
@@ -20,11 +20,11 @@
 
                 <li><a href="{{route('publications.posts.index')}}" class="navbar-link dropdown">Blog</a></li>
 
-                @if(config('app.env') == 'production')
-                    <li><a href="https://have-rod-will-travel.fireside.fm" class="navbar-link">Podcast</a></li>
-                @else
-                    <li><a href="#" class="navbar-link">Podcast</a></li>
-                @endif
+                {{--@if(config('app.env') == 'production')--}}
+                    {{--<li><a href="https://have-rod-will-travel.fireside.fm" class="navbar-link">Podcast</a></li>--}}
+                {{--@else--}}
+                    {{--<li><a href="#" class="navbar-link">Podcast</a></li>--}}
+                {{--@endif--}}
 
                 <li><a href="/maps/world" class="navbar-link">Map</a></li>
             </ul>
